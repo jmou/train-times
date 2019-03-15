7df970812f07ff434789a1be3813f25008858dd8
isaaacp <pearl.isaac@gmail.com>
2019-03-14 22:56:48 -0400

    finished react components

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,27 +3,68 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types'
 
+class App extends React.Component {
+	render() {
+		return(
+			<div>
+				<ListHeader/>
+				<Arrivals/>
+			</div>
+		);
+	}
+};
+
 const ListHeader = () => (
-		<div>
-			This is the header.
+		<div className="list-header">
+			DEPARTURES
 		</div>
 );
 
+class Arrivals extends React.Component {
+	state = {
+		arrivals:[
+			{time: 4, line: "2", station: 233, direction: 'N'},
+			{time: 6, line: "2", station: 233, direction: 'N'},
+			{time: 12, line: "2", station: 233, direction: 'N'},
+		]
+	};
+		
+	render() {
+		return(
+			<div>
+				<Header/>
+				<ArrivalsList arrivals={this.state.arrivals}/>
+			</div>
+		);
+	}
+};
+
 const ArrivalsList = ({ arrivals }) => (
-	<table className="arrivals-list">
-		<tbody>
-			{arrivals.map(arrival => <ArrivalsListItem key={arrival.time} station={arrival.station}/>)}
-		</tbody>
-	</table>
+	<div className="arrivals-list">
+		{arrivals.map(
+			//replace key with actual ID from api, pass in all info
+			arrivalInfo => <Arrival key={arrivalInfo.time} arrivalInfo={arrivalInfo}/>		
+		)}
+	</div>
 );
 
-const ArrivalsListItem = ({ station }) => (
-	<td>
-		{station}
-	</td>
+const Header = () => (
+	<div className="arrivals-header">
+		<span>Time</span>
+		<span>Line</span>
+		<span>Direction</span>
+		<span>Station</span>
+	</div>
 );
 
-const ArrivalIcon = 0;
+const Arrival = ({ arrivalInfo }) => (
+	//fix this JSX to return seperate elements for each thing to display
+	<div className="arrival-info">
+		<span className="station-name">{arrivalInfo.station}</span>
+		<span className="train-line">{arrivalInfo.line}</span>
+		<span className="train-direction">{arrivalInfo.direction}</span>
+	</div>
+);
 
 const testArrivals = [
 	{time: 4, line: "2", station: 233, direction: 'N'},
@@ -32,8 +73,5 @@ const testArrivals = [
 ]
 
 ReactDOM.render(
-	<div>
-		<ListHeader/>
-		<ArrivalsList arrivals = {testArrivals}/>
-	</div>
+	<App/>
 	, document.getElementById('root'));

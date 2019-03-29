f4dab982e80bcf8a27da13a1ed251dea89fd7548
isaaacp <pearl.isaac@gmail.com>
2019-03-29 16:53:51 -0400

    fixed css and train line icon display

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types'
 
+function importAll(r) {
+	let images = {};
+	r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+	return images;
+}
+
+const subwayIcons = importAll(require.context('./subway_img', false, /\.(png|jpe?g|svg)$/));
+
 class App extends React.Component {
 	render() {
 		return(
@@ -15,20 +23,20 @@ class App extends React.Component {
 };
 
 const ListHeader = () => (
-		<div className="list-header">
-			DEPARTURES
-		</div>
+	<div className="list-header">
+		DEPARTURES
+	</div>
 );
 
 class Arrivals extends React.Component {
 	state = {
 		arrivals:[
-			{time: 4, line: "2", station: 233, direction: 'N'},
-			{time: 6, line: "2", station: 233, direction: 'N'},
-			{time: 12, line: "2", station: 233, direction: 'N'},
+			{time: 4, line: "F", station: "Hoyt", direction: 'N'},
+			{time: 6, line: "A", station: "DeKalb", direction: 'S'},
+			{time: 12, line: "Q", station: "Jay", direction: 'N'},
 		]
 	};
-		
+
 	render() {
 		return(
 			<div>
@@ -50,27 +58,21 @@ const ArrivalsList = ({ arrivals }) => (
 
 const Header = () => (
 	<div className="arrivals-header">
-		<span>Time</span>
-		<span>Line</span>
-		<span>Direction</span>
-		<span>Station</span>
 	</div>
 );
 
 const Arrival = ({ arrivalInfo }) => (
 	//fix this JSX to return seperate elements for each thing to display
 	<div className="arrival-info">
-		<span className="station-name">{arrivalInfo.station}</span>
-		<span className="train-line">{arrivalInfo.line}</span>
-		<span className="train-direction">{arrivalInfo.direction}</span>
+		<span className="arrival-time">{arrivalInfo.time} MIN</span>
+		<LineIcon>{arrivalInfo.line}</LineIcon>
+		<span className="station-name">DEPARTING {arrivalInfo.station} FOR ANOTHER PLACE</span>
 	</div>
 );
 
-const testArrivals = [
-	{time: 4, line: "2", station: 233, direction: 'N'},
-	{time: 6, line: "2", station: 233, direction: 'N'},
-	{time: 12, line: "2", station: 233, direction: 'N'},
-]
+const LineIcon = ({ children }) => (
+	<img className="line-icon" src={subwayIcons[`${children}_bullet.png`]}/>
+);
 
 ReactDOM.render(
 	<App/>
